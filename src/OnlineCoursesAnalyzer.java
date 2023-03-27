5be8bfdbff78b0ac5bdf5f6fee01439031f9e312
yqxzy <11910538@mail.sustech.edu.cn>
2023-03-27 17:14:04 +0800

    CheckStyle modification

--- src/OnlineCoursesAnalyzer.java
+++ src/OnlineCoursesAnalyzer.java
@@ -17,7 +17,6 @@ import java.util.stream.Collectors;
 public class OnlineCoursesAnalyzer {
 
     List<Course> courses = new ArrayList<>();
-
     public OnlineCoursesAnalyzer(String datasetPath) {
         BufferedReader br = null;
         String line;
@@ -84,7 +83,6 @@ public class OnlineCoursesAnalyzer {
                         temp=temp.stream().distinct().collect(Collectors.toList());
                         map1.put(s[i], temp);
                     }else {
-
                         map1.put(s[i], x.getValue(). stream().distinct().collect(Collectors.toList()));
                     }
                 }
@@ -222,29 +220,29 @@ public class OnlineCoursesAnalyzer {
 }
 
 class Course {
-    String institution;
-    String number;
-    Date launchDate;
-    String title;
-    String instructors;
-    String subject;
-    int year;
-    int honorCode;
-    int participants;
-    int audited;
-    int certified;
-    double percentAudited;
-    double percentCertified;
-    double percentCertified50;
-    double percentVideo;
-    double percentForum;
-    double gradeHigherZero;
-    double totalHours;
-    double medianHoursCertification;
-    double medianAge;
-    double percentMale;
-    double percentFemale;
-    double percentDegree;
+  String institution;
+  String number;
+  Date launchDate;
+  String title;
+  String instructors;
+  String subject;
+  int year;
+  int honorCode;
+  int participants;
+  int audited;
+  int certified;
+  double percentAudited;
+  double percentCertified;
+  double percentCertified50;
+  double percentVideo;
+  double percentForum;
+  double gradeHigherZero;
+  double totalHours;
+  double medianHoursCertification;
+  double medianAge;
+  double percentMale;
+  double percentFemale;
+  double percentDegree;
 
     public Course(String institution, String number, Date launchDate,
                   String title, String instructors, String subject,
@@ -255,34 +253,34 @@ class Course {
                   double totalHours, double medianHoursCertification,
                   double medianAge, double percentMale, double percentFemale,
                   double percentDegree) {
-        this.institution = institution;
-        this.number = number;
-        this.launchDate = launchDate;
-        if (title.startsWith("\"")) title = title.substring(1);
-        if (title.endsWith("\"")) title = title.substring(0, title.length() - 1);
-        this.title = title;
-        if (instructors.startsWith("\"")) instructors = instructors.substring(1);
-        if (instructors.endsWith("\"")) instructors = instructors.substring(0, instructors.length() - 1);
-        this.instructors = instructors;
-        if (subject.startsWith("\"")) subject = subject.substring(1);
-        if (subject.endsWith("\"")) subject = subject.substring(0, subject.length() - 1);
-        this.subject = subject;
-        this.year = year;
-        this.honorCode = honorCode;
-        this.participants = participants;
-        this.audited = audited;
-        this.certified = certified;
-        this.percentAudited = percentAudited;
-        this.percentCertified = percentCertified;
-        this.percentCertified50 = percentCertified50;
-        this.percentVideo = percentVideo;
-        this.percentForum = percentForum;
-        this.gradeHigherZero = gradeHigherZero;
-        this.totalHours = totalHours;
-        this.medianHoursCertification = medianHoursCertification;
-        this.medianAge = medianAge;
-        this.percentMale = percentMale;
-        this.percentFemale = percentFemale;
-        this.percentDegree = percentDegree;
-    }
+    this.institution = institution;
+    this.number = number;
+    this.launchDate = launchDate;
+    if (title.startsWith("\"")) title = title.substring(1);
+    if (title.endsWith("\"")) title = title.substring(0, title.length() - 1);
+    this.title = title;
+    if (instructors.startsWith("\"")) instructors = instructors.substring(1);
+    if (instructors.endsWith("\"")) instructors = instructors.substring(0, instructors.length() - 1);
+    this.instructors = instructors;
+    if (subject.startsWith("\"")) subject = subject.substring(1);
+    if (subject.endsWith("\"")) subject = subject.substring(0, subject.length() - 1);
+    this.subject = subject;
+    this.year = year;
+    this.honorCode = honorCode;
+    this.participants = participants;
+    this.audited = audited;
+    this.certified = certified;
+    this.percentAudited = percentAudited;
+    this.percentCertified = percentCertified;
+    this.percentCertified50 = percentCertified50;
+    this.percentVideo = percentVideo;
+    this.percentForum = percentForum;
+    this.gradeHigherZero = gradeHigherZero;
+    this.totalHours = totalHours;
+    this.medianHoursCertification = medianHoursCertification;
+    this.medianAge = medianAge;
+    this.percentMale = percentMale;
+    this.percentFemale = percentFemale;
+    this.percentDegree = percentDegree;
+  }
 }
